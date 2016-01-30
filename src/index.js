39812b2d2eae7eadd3af3a4e6a75b6b91a6bf1fe
savelichalex <savelichalex93@gmail.com>
2016-01-30 16:44:03 +0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -24,3 +24,61 @@ app.listen(port, function() {
 
 const games = new Map();
 const firstKey = '0001';
+
+app.io.user(function* (next) {
+	console.log('someone connected');
+
+	yield next;
+
+	//on disconnect
+	if(this.joined) {
+		//delete from gamepads if needed
+
+		games.get(this.key).emit('user left', this.username);
+	}
+});
+
+//Routes
+
+app.io.router('create game', function* (next) {
+	function createKey() {
+		const key = Math.floor(Math.random() * 1000);
+		if(games.has(key)) {
+			return createKey;
+		} else {
+			return key;
+		}
+	}
+	const key = createKey();
+	games.set(key, this);
+
+	this.emit.('game created', key);
+});
+
+app.io.route('join game', function* (next, {key, username}) {
+	if(games.has(key)) {
+		this.joined = true;
+		this.key = key;
+		this.username = username;
+
+		games.get(key).emit('user joined'. username);
+	}
+});
+
+app.io.route('move', function* (next, {direction}) {
+	if(this.joined) {
+		games.get(this.key).emit('move', {
+			username: this.username,
+			direction
+		});
+	}
+});
+
+app.io.route('action', function* (next, {action}) {
+	if(this.joined) {
+		games.get(this.key).emit('action', {
+			username: this.username,
+			action
+		});
+	}
+})
