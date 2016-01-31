06c20d7de58d25623020cc46ae0f7a0ce327410a
amurashev <andreymurashev@gmail.com>
2016-01-31 15:32:43 +0300

    Add bot image

--- src/assets/desktop/map-template.js
+++ src/assets/desktop/map-template.js
@@ -1,7 +1,7 @@
 import { div, h1, img } from '@cycle/dom';
 
 export function getInitialMapTemplate(key) {
-	return div([
+	return div('.map-wrapper', [
 		img({src: '/src/static/img/map.png'}),
 		div('#map-console', [
 			h1('.game-key', 'District id: ' + [key])
